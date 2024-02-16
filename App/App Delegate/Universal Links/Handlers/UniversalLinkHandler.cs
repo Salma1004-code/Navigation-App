09433f5ec769bf845e0ba4f7640f3548f14fafce
mhdahab <67865770+mhdahab@users.noreply.github.com>
2024-02-16 12:57:35 +0200

    Delete App/App Delegate/Universal Links/Handlers directory

--- App/App Delegate/Universal Links/Handlers/UniversalLinkHandler.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-public interface UniversalLinkHandler
-{
-    // Declare a method for handling universal links
-    void HandleUniversalLink(List<UriQueryItem> queryItems, UniversalLinkVersion version);
-}
